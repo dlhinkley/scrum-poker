10b0b8546de17b90d86ea82ac301dca4becbb4e3
duane_hinkley <duane.hinkley@mheducation.com>
2021-02-20 10:51:06 -0800

    Game basically works.  Clean up display

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -5,7 +5,9 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { DatabaseService } from '../database.service';
 import { GameService } from '../game.service';
 import { Game } from '../game';
+import { Card } from '../card';
 import { User } from '../user';
+import { Point } from '../point';
 
 @Component({
     selector: 'app-play',
@@ -18,7 +20,9 @@ export class PlayComponent implements OnInit {
     users: User[] = <User[]>{};
     game: Game = <Game>{};
     user: User = <User>{};
-    points = [0, 1, 2, 3, 5, 8, 13, 21, 34];
+    cards: Card[] = <Card[]>[];
+    points: Point[] = <Point[]>[];
+    defaults = [0, 1, 2, 3, 5, 8, 13, 21, 34];
     average: number  = 0;
 
     constructor(
@@ -29,24 +33,53 @@ export class PlayComponent implements OnInit {
         private firestore: AngularFirestore,
     ) { 
         console.log('play');
+
+        this.defaults.forEach((v) => {
+            const point = <Point> {value: v};
+            this.points.push(point);
+        });
     }
-    setPoints(event: any, points: number): void {
-        this.databaseService.setPoints(this.userId, points)
+    updateCards() {
+       this.cards = <Card[]>[];
+       this.users.forEach(user => {
+           let display = '';
+           // In show mode, display points or X
+           if (this.game.show) {
+               display = (user.points > -1 ? user.points + '' : 'X');
+           } else {
+               display = (user.points > -1  ? '&#x2713;' : '?');
+           }
+           const selected = (user.points > -1);
+           const card = <Card> {
+               user: user,
+               display: display,
+               selected: selected,
+           }
+           this.cards.push(card);
+       });
+    }
+    updatePoints() {
+        this.points.forEach(point => {
+            point.selected = (point.value === this.user.points);
+        });
+    }
+    setPoints(event: any, p: number): void {
+        this.points.forEach(point => {
+            point.selected = false;
+        });
+        this.updateCards();
+        this.points[p].selected = true;
+        this.databaseService.setPoints(this.userId, this.points[p].value)
     }
     clrPoints(event: any): void {
         this.databaseService.clrPoints(this.gameId);
     }
 
     togglePoints(event: any): void {
+        this.updateCards();
         this.databaseService.setShow(this.gameId, ! this.game.show);
     }
-    pointsSelected(point: number) {
-        let style = 'btn-outline-primary';
-        if (point == this.user.points) {
-            style = 'btn-primary';
-        }
-        return style;
-    }
+
     setAverage(): void {
         let count = 0;
         let sum = 0;
@@ -71,8 +104,8 @@ export class PlayComponent implements OnInit {
             if (!this.userId) {
                 this.router.navigate(['add-user'])
             }
-            // See if the game exists
 
+            // Get the game
             this.databaseService.getGame(this.gameId)
             .subscribe((doc) => {
                 // If game found
@@ -83,12 +116,15 @@ export class PlayComponent implements OnInit {
                     this.router.navigate(['add-game'])
                 }
             });
+
+            // Get the user
             this.databaseService.getUser(this.userId)
             .subscribe((doc) => {
-                // If game found
+                // If user found
                 if (doc.data()) {
                     console.log('this.user', doc.data());
                     this.user = <User> doc.data();
+                    this.updatePoints();
                     if (this.user.gameId !== this.gameId) {
                         this.router.navigate(['add-user'])
                     }
@@ -103,7 +139,10 @@ export class PlayComponent implements OnInit {
                 console.log('users=', users);
                 this.users = <User[]>users;
                 this.setAverage();
+                this.updateCards();
             });
+
+            // Load and watch game for changes
             this.databaseService.watchGame(this.gameId)
             .subscribe((doc) => {
                 console.log('games=', doc);
