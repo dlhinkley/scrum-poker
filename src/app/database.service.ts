0ffe3cb4d83b7977dbe9dd212d09991fa62380c5
duane_hinkley <duane.hinkley@mheducation.com>
2021-02-19 17:05:32 -0800

    Game basically works, but it's ugly.  Working on clearing cards

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 import { Game } from './game';
 import { User } from './user';
 
@@ -19,21 +21,44 @@ export class DatabaseService {
 				return ref.id;
 			});
   }
-   createUser(user: User) {
-    return this.firestore
+    createUser(user: User) {
+     return this.firestore
             .collection('user')
             .add(user)
             .then(ref => {
 				return ref.id;
 			});
-  }
- 
- 	getUsers(gameId: string): Observable<User[]> {
+   }
+   setShow(gameId: string, show: boolean) {
+     return this.firestore
+            .collection('game')
+			.doc(gameId)
+            .update({ show: show});
+   }
+   clrPoints(gameId: string) {
+
+	 const userRef = this.firestore
+	    .collection('user', ref => ref.where('gameId', '==', gameId));
+
+     batch.update(userRef, { points: -1});
+
+	 return natch.commit();
+   }
+   setPoints(userId: string, points: number) {
+     return this.firestore
+            .collection('user')
+			.doc(userId)
+            .update({ points: points});
+   }
+ 	getUsers(gameId: string): Observable<unknown[]> {
 	    return this.firestore.collection('user', ref => ref.where('gameId', '==', gameId)).valueChanges();
 	};
 
  	watchGame(gameId: string) {
-	    return this.firestore.collection('game').doc(gameId).valueChanges();
+	    return this.firestore.doc('game/' + gameId).valueChanges()
+	};
+ 	getUser(userId: string) {
+	    return this.firestore.collection('user').doc(userId).get()
 	};
  	getGame(gameId: string) {
 	    return this.firestore.collection('game').doc(gameId).get()
--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { DatabaseService } from '../database.service';
 import { GameService } from '../game.service';
 import { Game } from '../game';
@@ -13,40 +14,91 @@ import { User } from '../user';
 })
 export class PlayComponent implements OnInit {
   gameId: string = '';
-  users: Observable<any> = <Observable<any>>{};
-  game: Observable<any> = <Observable<any>>{};
+  userId: string = '';
+  users: User[] = <User[]>{};
+  game: Game = <Game>{};
+  user: User = <User>{};
+  points = [0, 1, 2, 3, 5, 8, 13, 21, 34];
+  average: number  = 0;
 
   constructor(
 	 private router: Router,
      private activatedRoute: ActivatedRoute,
      private gameService: GameService,
      private databaseService: DatabaseService,
+     private firestore: AngularFirestore,
   ) { 
 		  console.log('play');
+  }
+  setPoints(event: any, points: number): void {
+		this.databaseService.setPoints(this.userId, points)
+  }
+  clrPoints(event: any): void {
+		this.databaseService.clrPoints(this.gameId);
+  }
+
+  togglePoints(event: any): void {
+		this.databaseService.setShow(this.gameId, ! this.game.show);
+  }
+
+  setAverage(): void {
+    let count = 0;
+	let sum = 0;
+	this.users.forEach(user => {
+        if (user.points > -1) {
+		   sum += user.points;
+           count++;
+		}
+		this.average = Math.round(sum / count * 10) / 10;
+	});
   }
   ngOnInit(): void {
    this.activatedRoute.params.subscribe(parameter => {
       this.gameId = parameter.gameId;
 	  // If no gameid, navigate to create a game
 	  // If you don't have a user id yet, navigate to create a user 
-      let userId = this.gameService.getUserId();
-      if (!userId) {
+      this.userId = this.gameService.getUserId();
+      if (!this.userId) {
           this.router.navigate(['add-user'])
 	  }
-      // Load the game name
-      this.databaseService.getGame(this.gameId)
-      	.subscribe((doc) => {
+      // See if the game exists
+	  
+	  this.databaseService.getGame(this.gameId)
+	     .subscribe((doc) => {
             // If game found
 			if (doc.data()) {
 			   console.log('this.game', doc.data());
-		       this.game = this.databaseService.watchGame(this.gameId);	
 	           this.gameService.setGameId(this.gameId);
 			} else {
                this.router.navigate(['add-game'])
 			}
 	   });
+	  this.databaseService.getUser(this.userId)
+	     .subscribe((doc) => {
+            // If game found
+			if (doc.data()) {
+			   console.log('this.user', doc.data());
+	           this.user = <User> doc.data();
+			   if (this.user.gameId !== this.gameId) {
+                  this.router.navigate(['add-user'])
+			   }
+			} else {
+               this.router.navigate(['add-user'])
+			}
+	   });
+	  
 	   // Load the users (including changes)
-      this.users = this.databaseService.getUsers(this.gameId);
+      this.databaseService.getUsers(this.gameId)
+         .subscribe((users ) => {
+			  console.log('users=', users);
+		      this.users = <User[]>users;
+			  this.setAverage();
+	     });
+      this.databaseService.watchGame(this.gameId)
+	     .subscribe((doc) => {
+			  console.log('games=', doc);
+		      this.game = <Game>doc;
+		 });;
     });
   }
 }
