ce116dddad8d3530eef1c15764d17408e445e6df
duane_hinkley <duane.hinkley@mheducation.com>
2021-02-21 09:51:47 -0800

    Working on e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,18 +1,44 @@
 import { browser, logging } from 'protractor';
 import { AppPage } from './app.po';
+import { AddGamePage } from './app.add-game';
+import { AddUserPage } from './app.add-user';
+import { PlayPage } from './app.play';
 
 describe('workspace-project App', () => {
   let page: AppPage;
+  let addGame: AddGamePage;
+  let addUser: AddUserPage;
+  let play: PlayPage;
 
   beforeEach(() => {
     page = new AppPage();
+    addGame = new AddGamePage();
+    addUser = new AddUserPage();
+    play = new PlayPage();
   });
 
-  it('should display welcome message', async () => {
+  it('should accept game  name', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('scrum-poker app is running!');
+    expect(await addGame.getTitleText()).toEqual(addGame.title);
+
+    await addGame.setNameText();
+    await addGame.clickSubmit();
   });
 
+  it('should accept user  name', async () => {
+    await browser.sleep(5 * 1000)
+    expect(await addUser.getTitleText()).toEqual(addUser.title);
+    
+    await addUser.setNameText();
+    await addUser.clickSubmit();
+  });
+/*
+  it('should display play page', async () => {
+
+    expect(await play.getGameNameText()).toEqual(play.gameName);
+
+  });
+  */
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
