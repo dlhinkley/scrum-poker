30a8009fbb823e7e08eebaf394cca5a44f866e84
duane_hinkley <duane.hinkley@mheducation.com>
2021-02-20 17:53:16 -0800

    Add firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: 'AIzaSyA7pBjsxs0CkjLfiALy7b-ZPQkJ5tIxvBI',
+        authDomain: 'scrum-poker-5b610.firebaseapp.com',
+        databaseURL: 'https://scrum-poker-5b610-default-rtdb.firebaseio.com',
+        projectId: 'scrum-poker-5b610',
+        storageBucket: 'scrum-poker-5b610.appspot.com',
+        messagingSenderId: '735266807295',
+        appId: '1:735266807295:web:14836ab81945237d66dbca',
+        measurementId: 'G-P2L43B0QT5'
+    }
 };
